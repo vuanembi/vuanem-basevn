b4cbb83c85e73683350da37028f2856d375a79c7
HM <hieumdd@gmail.com>
2021-11-19 22:51:28 +0700

    feat: Added tasks

--- test/test_units.py
+++ test/test_units.py
@@ -6,13 +6,26 @@ from main import main
 from controller.tasks import TABLES
 
 
+def run(data):
+    return main(Mock(get_json=Mock(return_value=data), args=data))
+
+
 @pytest.mark.parametrize(
     "data",
     TABLES,
-    ids=[f"{i['resource']} || {i['table']}" for i in TABLES]
+    ids=[f"{i['resource']} || {i['table']}" for i in TABLES],
 )
 def test_pipelines(data):
-    res = main(Mock(get_json=Mock(return_value=data), args=data))["results"]
+    res = run(data)["results"]
     assert res["num_processed"] >= 0
     if res["num_processed"] > 0:
         assert res["num_processed"] == res["output_rows"]
+
+
+def test_tasks():
+    res = run(
+        {
+            "tasks": "basevn",
+        }
+    )
+    assert res["tasks"] > 0
