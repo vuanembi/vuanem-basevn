32fc0e43172c2a20696c1e0cb96765aa42553f75
HM <hieumdd@gmail.com>
2021-11-19 17:07:50 +0700

    feat: Updated Workflows

--- models/Workflow/Workflows.py
+++ models/Workflow/Workflows.py
@@ -4,139 +4,111 @@ from libs.workflow import get_workflows
 Workflows: Basevn = {
     "name": "Workflow_Workflows",
     "get": get_workflows,
-    "transform": lambda rows: rows,
-    "schema": [
-        {"name": "id", "type": "INTEGER"},
-        {"name": "type", "type": "STRING"},
-        {"name": "hid", "type": "STRING"},
-        {"name": "token", "type": "STRING"},
-        {"name": "status", "type": "INTEGER"},
-        {"name": "state", "type": "STRING"},
-        {"name": "starred", "type": "INTEGER"},
-        {"name": "name", "type": "STRING"},
-        {"name": "title", "type": "STRING"},
-        {"name": "content", "type": "STRING"},
-        {"name": "color", "type": "INTEGER"},
-        {"name": "followers", "type": "STRING", "mode": "REPEATED"},
-        {"name": "since", "type": "TIMESTAMP"},
-        {"name": "last_update", "type": "TIMESTAMP"},
-        {"name": "finish_at", "type": "TIMESTAMP"},
-        {"name": "stage_id", "type": "INTEGER"},
+    "transform": lambda rows: [
         {
-            "name": "stage_export",
-            "type": "RECORD",
+            "id": row.get("id"),
+            "name": row.get("name"),
+            "owners": row.get("owners"),
+            "followers": row.get("followers"),
+            "viewable_teams": row.get("viewable_teams"),
+            "creatable_teams": row.get("creatable_teams"),
+            "stages": [
+                {
+                    "id": stage.get("id"),
+                    "name": stage.get("name"),
+                    "metatype": stage.get("metatype"),
+                    "owners": stage.get("owners"),
+                    "followers": stage.get("followers"),
+                }
+                for stage in row["stage"]
+            ]
+            if row.get("stage", [])
+            else [],
+            "failed_reasons": [
+                {
+                    "key": failed_reason.get("key"),
+                    "value": failed_reason.get("value"),
+                }
+                for failed_reason in row["failed_reasons"]
+            ]
+            if row.get("failed_reasons", [])
+            else [],
+            "form": [
+                {
+                    "id": form.get("id"),
+                    "name": form.get("name"),
+                    "type": form.get("type"),
+                    "placeholder": form.get("placeholder"),
+                    "enabled": form.get("enabled"),
+                    "required": form.get("required"),
+                    "options": form.get("options"),
+                    "stage_id": form.get("stage_id"),
+                }
+                for form in row["form"]
+            ]
+            if row.get("form", [])
+            else [],
+            "stats": {
+                "done": row["stats"].get("done"),
+                "failed": row["stats"].get("failed"),
+                "active": row["stats"].get("active"),
+            }
+            if row.get("stats", {})
+            else {},
+        }
+        for row in rows
+    ],
+    "schema": [
+        {"name": "id", "type": "STRING"},
+        {"name": "name", "type": "STRING"},
+        {"name": "owners", "type": "STRING", "mode": "repeated"},
+        {"name": "followers", "type": "STRING", "mode": "repeated"},
+        {"name": "viewable_teams", "type": "STRING", "mode": "repeated"},
+        {"name": "creatable_teams", "type": "STRING", "mode": "repeated"},
+        {
+            "name": "stages",
+            "type": "record",
+            "mode": "repeated",
             "fields": [
-                {"name": "id", "type": "INTEGER"},
+                {"name": "id", "type": "STRING"},
                 {"name": "name", "type": "STRING"},
                 {"name": "metatype", "type": "STRING"},
-                {"name": "workflow_id", "type": "INTEGER"},
-                {"name": "type", "type": "STRING"},
+                {"name": "owners", "type": "STRING", "mode": "repeated"},
+                {"name": "followers", "type": "STRING", "mode": "repeated"},
             ],
         },
         {
-            "name": "stats",
-            "type": "RECORD",
+            "name": "failed_reasons",
+            "type": "record",
+            "mode": "repeated",
             "fields": [
-                {"name": "notes", "type": "INTEGER"},
-                {"name": "posts", "type": "INTEGER"},
-                {"name": "files", "type": "INTEGER"},
-                {"name": "likes", "type": "INTEGER"},
-            ],
-        },
-        {"name": "user_id", "type": "INTEGER"},
-        {"name": "username", "type": "STRING"},
-        {
-            "name": "todos",
-            "type": "RECORD",
-            "mode": "REPEATED",
-            "fields": [
-                {"name": "id", "type": "INTEGER"},
-                {"name": "name", "type": "STRING"},
-                {"name": "status", "type": "INTEGER"},
-                {"name": "stage_id", "type": "INTEGER"},
-                {"name": "deadline", "type": "TIMESTAMP"},
-                {"name": "username", "type": "STRING"},
-                {"name": "type", "type": "STRING"},
-                {"name": "link", "type": "STRING"},
-            ],
-        },
-        {"name": "creator_id", "type": "INTEGER"},
-        {"name": "creator_username", "type": "STRING"},
-        {"name": "deadline", "type": "INTEGER"},
-        {"name": "stage_deadline", "type": "TIMESTAMP"},
-        {"name": "stage_start", "type": "TIMESTAMP"},
-        {"name": "workflow_id", "type": "INTEGER"},
-        {
-            "name": "workflow_export",
-            "type": "RECORD",
-            "fields": [
-                {"name": "id", "type": "INTEGER"},
-                {"name": "name", "type": "STRING"},
-                {"name": "type", "type": "STRING"},
-            ],
-        },
-        {
-            "name": "moves",
-            "type": "RECORD",
-            "mode": "REPEATED",
-            "fields": [
-                {"name": "id", "type": "INTEGER"},
-                {"name": "user_id", "type": "INTEGER"},
-                {"name": "mover_id", "type": "INTEGER"},
-                {"name": "job_id", "type": "INTEGER"},
-                {"name": "stage_id", "type": "INTEGER"},
-                {"name": "stage_start", "type": "TIMESTAMP"},
-                {"name": "stage_deadline", "type": "TIMESTAMP"},
-                {"name": "from_stage_id", "type": "INTEGER"},
-                {"name": "duration", "type": "INTEGER"},
-                {"name": "past", "type": "INTEGER"},
-                {"name": "stage_end", "type": "TIMESTAMP"},
-            ],
-        },
-        {
-            "name": "acl",
-            "type": "RECORD",
-            "fields": [
-                {"name": "view", "type": "INTEGER"},
-                {"name": "status", "type": "INTEGER"},
-                {"name": "managed", "type": "INTEGER"},
-                {"name": "edit", "type": "INTEGER"},
+                {"name": "key", "type": "STRING"},
+                {"name": "value", "type": "STRING"},
             ],
         },
         {
             "name": "form",
-            "type": "RECORD",
-            "mode": "REPEATED",
+            "type": "record",
+            "mode": "repeated",
             "fields": [
                 {"name": "id", "type": "STRING"},
                 {"name": "name", "type": "STRING"},
-                {"name": "value", "type": "STRING"},
                 {"name": "type", "type": "STRING"},
                 {"name": "placeholder", "type": "STRING"},
+                {"name": "enabled", "type": "INTEGER"},
+                {"name": "required", "type": "STRING"},
+                {"name": "options", "type": "STRING", "mode": "repeated"},
+                {"name": "stage_id", "type": "STRING"},
             ],
         },
         {
-            "name": "on_failed",
-            "type": "RECORD",
+            "name": "stats",
+            "type": "record",
             "fields": [
-                {"name": "stage_id", "type": "INTEGER"},
-                {"name": "failed_reason_id", "type": "STRING"},
-                {"name": "failed_name", "type": "STRING"},
-                {"name": "note", "type": "STRING"},
-                {"name": "since", "type": "TIMESTAMP"},
+                {"name": "done", "type": "STRING"},
+                {"name": "failed", "type": "STRING"},
+                {"name": "active", "type": "STRING"},
             ],
         },
-        {
-            "name": "data",
-            "type": "RECORD",
-            "fields": [
-                {"name": "deadline_by_timesheet", "type": "INTEGER"},
-                {"name": "first_assignee", "type": "INTEGER"},
-                {"name": "last_assignee", "type": "INTEGER"},
-            ],
-        },
-        {"name": "keyword", "type": "STRING"},
-        {"name": "deadline_by_timesheet", "type": "INTEGER"},
-        {"name": "first_assignee", "type": "INTEGER"},
     ],
 }
